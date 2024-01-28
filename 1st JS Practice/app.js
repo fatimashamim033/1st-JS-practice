6374c5d6bf233141546b36511b7b77bffcbf81ce
fatima shamim <fatimashamim033@gmail.com>
2024-01-28 15:42:40 +0500

    Add files via upload

---
+++ 1st JS Practice/app.js
@@ -0,0 +1,38 @@
+var a=+prompt('enter value 1')
+var b=+prompt('enter value 2')
+
+var add=a+b
+var sub=a-b
+var mul=a*b
+var div=a/b
+document.write(
+    '<table border= 1px>'+
+    '<tr>' +
+    '<th>'+'ADDITION' + '</th>' +
+    '</tr>' +
+    '<tr>' +
+    '<td>' + add + '</td>' + 
+    '</tr>'+
+    '<tr>'+
+    '<th>'+'SUBTRACTION' + '</th>' +
+    '</tr>' +
+    '<tr>' +
+    '<td>' + sub + '</td>' + 
+    '</tr>'+
+    '<tr>'+
+    '<th>'+'MULTIPLICATION' + '</th>' +
+    '</tr>' +
+    '<tr>' +
+    '<td>' + mul + '</td>' + 
+    '</tr>'+
+    '<tr>'+
+    '<th>'+'DIVISION' + '</th>' +
+    '</tr>' +
+    '<tr>' +
+    '<td>' + div + '</td>' + 
+    '</tr>'+
+    '</th>'
+
+)
+
+alert 
